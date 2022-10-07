af59667e4a39dda02e57c44cc18661d2745decad
Matt602 <matthew.frierson062@gmail.com>
2022-10-07 01:32:50 -0500

    Created the object constructor Book and a function to add books to array

---
+++ myScript.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+
+function Book(name, author, pages, readStatus) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+
+function addBookToLibrary() {
+
+}
