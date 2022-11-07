164a7dc94282cf66650460e135d65aa598d3bb0b
Matt602 <matthew.frierson062@gmail.com>
2022-11-06 22:22:37 -0600

    Implemented a form element to pop up when clicking on the new book button.

--- myScript.js
+++ myScript.js
@@ -1,15 +1,46 @@
 let myLibrary = [];
 
 
-function Book(name, author, pages, readStatus) {
+function Book(name, author) {//, pages, readStatus) {
     this.name = name;
     this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
-    this.index = myLibrary.length;
+    //this.pages = pages;
+    //this.readStatus = readStatus;
+    //this.index = myLibrary.length;
 }
 
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function createForm() {
+    const container = document.querySelector('#container');
+    let form = document.createElement('form');
+    form.setAttribute("action", "http://httpbin.org/post");
+    form.setAttribute("method", "POST")
+    let titleLabel = document.createElement('label');
+    titleLabel.setAttribute("for", "title");
+    titleLabel.textContent = "Book title ";
+    let titleInput = document.createElement('input');
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("id", "title");
+    titleInput.setAttribute("name", "title");
+    titleInput.setAttribute("required", "")
+    let submitButton = document.createElement('button');
+    submitButton.setAttribute("type", "submit");
+    submitButton.textContent = "Confirm";
+
+    form.appendChild(titleLabel);
+    form.appendChild(titleInput);
+    form.appendChild(submitButton);
+    container.appendChild(form);
+}
+
+// event listener for button to bring up form to put info for
+// new books
+
+const nButton = document.querySelector('.new-book');
+nButton.addEventListener('click', () => {
+    createForm();
+});
