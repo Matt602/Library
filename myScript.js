7d9625e7e311526d3291e07e20dbee365fa49cfb
Matt602 <matthew.frierson062@gmail.com>
2022-12-09 22:00:47 -0600

    Added the author field for the book's data as well as started working on the read status.

--- myScript.js
+++ myScript.js
@@ -2,11 +2,11 @@ let myLibrary = [];
 
 
 
-function Book(name) {//, pages, readStatus) {
+function Book(name, author, readStatus) {//, pages, readStatus) {
     this.title = name;
-    //this.author = author;
+    this.author = author;
     //this.pages = pages;
-    //this.readStatus = readStatus;
+    this.readStatus = readStatus;
     //this.index = myLibrary.length;
 }
 
@@ -14,7 +14,9 @@ function Book(name) {//, pages, readStatus) {
 function addBookToLibrary(event) {
     event.preventDefault();
     let title = document.getElementById('title').value;
-    let b = new Book(title);
+    let author = document.getElementById('author').value;
+    let readStatus = 'no';
+    let b = new Book(title, author, readStatus);
     myLibrary.push(b);
     deleteForm();
     clearTable();
@@ -31,6 +33,8 @@ function createForm() {
     form.setAttribute("method", "POST");
     form.setAttribute("id", "myForm")
     form.setAttribute("type", "submit")
+
+
     let titleLabel = document.createElement('label');
     titleLabel.setAttribute("for", "title");
     titleLabel.textContent = "Book title ";
@@ -38,13 +42,27 @@ function createForm() {
     titleInput.setAttribute("type", "text");
     titleInput.setAttribute("id", "title");
     titleInput.setAttribute("name", "title");
-    titleInput.setAttribute("required", "")
+    titleInput.setAttribute("required", "");
+
+    let authorLabel = document.createElement('label');
+    authorLabel.setAttribute("for", "author");
+    authorLabel.textContent = "Author ";
+    let authorInput = document.createElement('input');
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("id", "author");
+    authorInput.setAttribute("name", "author");
+    authorInput.setAttribute("required", "");
+
+
+
     let submitButton = document.createElement('button');
     submitButton.setAttribute("onclick", "addBookToLibrary(event)");
     submitButton.textContent = "Add Book";
 
     form.appendChild(titleLabel);
     form.appendChild(titleInput);
+    form.appendChild(authorLabel);
+    form.appendChild(authorInput);
     form.appendChild(submitButton);
     container.appendChild(form);
 }
@@ -67,10 +85,22 @@ function displayBooks() {
         newBook.setAttribute("class", "bookData");
         let title = document.createElement('td');
         title.textContent = myLibrary[i]['title'];
-        //let author = document.createElement('td');
-        //author.textContent = myLibrary[i]['author'];
+        let author = document.createElement('td');
+        author.textContent = myLibrary[i]['author'];
+        let readStatus = document.createElement('td');
+        let sub = document.createElement('p');
+        sub.innerHTML = myLibrary[i]['readStatus'];
+        readStatus.appendChild(sub);
+
+        
+        
+
+
+
+
         newBook.appendChild(title);
-        //newBook.appendChild(author);
+        newBook.appendChild(author);
+        newBook.appendChild(readStatus);
         table.appendChild(newBook);
     }
 
